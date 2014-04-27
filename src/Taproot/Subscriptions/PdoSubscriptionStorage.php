94aaaef646750f7e6bfd626328d1feef154fb384
Barnaby Walters <barnaby@waterpigs.co.uk>
2014-04-27 21:55:43 +0000

    Tried to make table generation work for postgres too

--- src/Taproot/Subscriptions/PdoSubscriptionStorage.php
+++ src/Taproot/Subscriptions/PdoSubscriptionStorage.php
@@ -91,6 +91,7 @@ class PdoSubscriptionStorage implements SubscriptionStorage {
 
 // In the future, if changes are made, detect the version change and migrate accordingly.
 function migratePdoSubscriptionStorageTables(PDO $pdo, $prefix='') {
+	$idColumnDefinition = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME) == 'pgsql' ? 'int' : 'int(11)';
 	$result = $pdo->exec(<<<EOT
 CREATE TABLE IF NOT EXISTS {$prefix}config (
 	key varchar(100) NOT NULL,
@@ -99,7 +100,7 @@ CREATE TABLE IF NOT EXISTS {$prefix}config (
 );
 
 CREATE TABLE IF NOT EXISTS {$prefix}subscriptions (
-	id int(11) NOT NULL,
+	id {$idColumnDefinition} NOT NULL,
 	created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	last_updated timestamp NULL DEFAULT NULL,
 	last_pinged timestamp NULL DEFAULT NULL,
@@ -112,7 +113,7 @@ CREATE TABLE IF NOT EXISTS {$prefix}subscriptions (
 
 CREATE TABLE IF NOT EXISTS {$prefix}pings (
 	id varchar(500) NOT NULL,
-	subscription int(11) NOT NULL,
+	subscription {$idColumnDefinition} NOT NULL,
 	datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	content_type varchar(100) NOT NULL,
 	content text NOT NULL,
